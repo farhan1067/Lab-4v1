a6e06099ad9b306ae733e81dc0cecd4e47793f46
mdfarhanlabib <mdfarhanlabib@mc6045-34.labs.sce.carleton.ca>
2018-10-05 19:35:49 -0400

    Hello

---
+++ lab4/src/Hello.java
@@ -0,0 +1,19 @@
+
+public class Hello {
+
+	public static void main(String[] args) 
+	{
+		Hello hello = new Hello();
+		hello.methodA();
+		hello.methodB();
+		hello.methodC();
+	}
+
+	public void methodA( ) { }
+
+	public void methodB() {
+		System.out.println("Checking....");
+	}
+
+	public void methodC() { }
+}
